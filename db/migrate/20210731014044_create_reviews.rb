b1913cf08cb9c2c669ec730e0717160fe08df54f
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-07-30 21:54:18 -0400

    databases created and first migration made

--- db/migrate/20210731014044_create_reviews.rb
+++ db/migrate/20210731014044_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-
+      t.string :title
+      t.text :content 
+      t.integer :user_id
+      t.integer :zoo_id
       t.timestamps
     end
   end
--- db/migrate/20210731014111_create_zoos.rb
+++ db/migrate/20210731014111_create_zoos.rb
@@ -1,7 +1,8 @@
 class CreateZoos < ActiveRecord::Migration[6.1]
   def change
     create_table :zoos do |t|
-
+      t.string :name 
+      t.integer :location_id 
       t.timestamps
     end
   end
