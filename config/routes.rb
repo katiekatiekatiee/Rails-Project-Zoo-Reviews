95e404bdfe17bc3fd8e04705f45e517c500648b2
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-02 20:46:52 -0400

    added routes in config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  
+  delete '/logout', to: 'sessions#destroy'
+
   resources :locations
   resources :zoos
   resources :reviews
