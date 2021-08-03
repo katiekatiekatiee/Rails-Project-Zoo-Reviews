9794e0be542ecb75c62bfa4d0efcbae900866b6c
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-02 20:31:49 -0400

    seed data with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+data = RestClient.get 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-cities-demographics&q=&facet=city&facet=state&facet=race'
+# byebug
+location_data_array = JSON.parse(data)["records"]["fields"]
+location_data_array.each do |location|
+    Location.create(state: location["state"])
+end
