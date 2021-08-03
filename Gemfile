6cd45bfeaf7882b52d4edfac603a3a9cd4f25cb3
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-03 19:43:44 -0400

    user signup fully functioning

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'rest-client'
+gem 'city-state'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -39,7 +40,7 @@ group :development do
   gem 'web-console', '>= 4.1.0'
   # Display performance information such as SQL time and flame graphs for each request in your browser.
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
-  gem 'rack-mini-profiler', '~> 2.0'
+  #gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-data = RestClient.get 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-cities-demographics&q=&facet=city&facet=state&facet=race'
-# byebug
-location_data_array = JSON.parse(data)["records"]["fields"]
-location_data_array.each do |location|
-    Location.create(state: location["state"])
-end
+# data = RestClient.get 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-cities-demographics&q=&facet=city&facet=state&facet=race'
+# # byebug
+# location_data_array = JSON.parse(data)["records"]["fields"]
+# location_data_array.each do |location|
+#     Location.create(state: location["state"])
+# end   THIS DOESNT WORK
+
+# states = CS.states(:us)
+# states.each do |key, value|
+#     Location.find_or_create_by(state: value)
+# end     https://github.com/loureirorg/city-state
+
+#User.create(username: "Captain Squiggles", email: "ahoymate22@aol.com", password: "captainsquiggs22" )
+# User.create(username: "Ellie", email: "catsruledogsdrool@gmail.com", password: "snacks247" )
+# User.create(username: "Happy the Clown", email: "clownshoes@yahoo.com", password: "popcorn1911" )
+# User.create(username: "Scott", email: "lettucehead45@outlook.com", password: "ilovelettuce24" )
+
