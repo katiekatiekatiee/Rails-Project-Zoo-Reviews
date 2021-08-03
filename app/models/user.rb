ead194974c45c7fcd39dd936ace186c46e1b9b59
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-02 20:52:47 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password 
     has_many :reviews
     has_many :zoos, through: :reviews
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true 
+    validates :password, length: { minimum: 8 }
 end
