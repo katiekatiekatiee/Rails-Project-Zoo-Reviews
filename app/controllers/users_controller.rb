3eb25e8a02ff7ca646e629a9ebc5f6998e52df79
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-02 20:57:45 -0400

    filled out user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new 
+        end
     end
 
     def show
+        @user = User.find_by_id(params[:id])
     end
    
+
+   private
+    def user_params
+        params.require(:user).permit(:email, :username, :password)
+    end
+
 end
