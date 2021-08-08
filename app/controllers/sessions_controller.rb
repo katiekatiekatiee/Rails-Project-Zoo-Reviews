3e80fa5fe7ed11ef55677a57c5d6ab94003b94bb
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-08-08 16:50:46 -0400

    edited naigation and zoo show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by_email(params[:user][:email])
+        #byebug
         if user && user.authenticate(params[:user][:password])
             session[:user_id] = user.id
             redirect_to zoos_path
@@ -33,7 +34,7 @@ class SessionsController < ApplicationController
         #byebug
         if user.valid? 
             session[:user_id] = user.id
-            flash[:message] = "Login Successful"
+            
             redirect_to zoos_path
         end
     end
